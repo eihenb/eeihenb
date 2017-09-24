c759e15dd4ffab24fe25e800fb6c4fa249a77587
Eddie Eihenbaums <eihenb3333@gmail.com>
2017-09-24 10:15:09 +0100

    added task 4087

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,11 @@
 package ru.job4j;
 
+/**
+* Class Calculate
+* author Eduards
+* since 24.09.2017
+*/
+
 public class Calculate {
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
